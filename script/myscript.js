b43e490b8719b2b101711026e0131f117ee1f0f1
KW8Git <3100658588@qq.com>
2016-05-18 16:33:46 +0800

    add analytor of brower in script folder

--- script/myscript.js
+++ script/myscript.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
     $("header .logo_container .logo").show().addClass("animated zoomInDown");
     $("#main .content h2").delay(3000).show(3000);
     $("#main .content p").delay(3000).show(3000);
+    
+    /*判断浏览器类型*/
+    var browerType = window.navigator.userAgent.indexOf("Chrome") !== -1;
+    if(browerType){
+    	/*显示img*/
+    	$(".big_bg_img img").show();
+    }else{
+    	    /*添加brower-noChrome*/
+   		$("#big_container").addClass("brower-noChrome");
+    }
 })
