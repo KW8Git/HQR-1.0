02893a425090528c15c9a4ccc228cd0d71160286
KW8Git <3100658588@qq.com>
2016-05-19 01:23:49 +0800

    add codes about Edge brower

--- script/myscript.js
+++ script/myscript.js
@@ -6,12 +6,18 @@ $(document).ready(function(){
     $("#main .content p").delay(3000).show(3000);
     
     /*判断浏览器类型*/
-    var browerType = window.navigator.userAgent.indexOf("Chrome") !== -1;
-    if(browerType){
-    	/*显示img*/
-    	$(".big_bg_img img").show();
+    var isChrome = window.navigator.userAgent.indexOf("Chrome") !== -1;
+    var isEdge = window.navigator.userAgent.indexOf("Edge") !== -1;
+    if(isChrome){
+    	if(isEdge){
+    		/*添加brower-noChrome*/
+    		$("#big_container").addClass("brower-noChrome");
+    	}else{
+    		/*显示img*/
+    		$(".big_bg_img img").show();
+    	}
     }else{
-    	    /*添加brower-noChrome*/
+    	/*添加brower-noChrome*/
    		$("#big_container").addClass("brower-noChrome");
     }
 })
